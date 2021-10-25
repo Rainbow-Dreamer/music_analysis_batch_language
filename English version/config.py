c34f7cdc81f66f48aeec6fe8dbd99debcff5d7b8
Rainbow <2180502841@qq.com>
2021-10-26 00:36:47 +0800

    update

---
+++ English version/config.py
@@ -0,0 +1,26 @@
+﻿config_dict = {
+    'eachline_character': 35,
+    'pairing_symbols': ['()', '[]', "''", '""'],
+    'wraplines_number': 2,
+    'font_type': 'Consolas',
+    'font_size': 12,
+    'background_mode': 'white',
+    'grammar_highlight': {
+        'red': [';'],
+        'blue': ['$'],
+        'violet': ['ca=', 'c=', 's=', 'b=', 'a=', 'i='],
+        'green': ['k.'],
+        'deep pink': ['k!']
+    },
+    'background_color': 'white',
+    'foreground_color': 'black',
+    'active_background_color': 'ivory2',
+    'day_and_night_colors': ('white', 'gray8'),
+    'search_highlight_color': ['light blue', 'light green'],
+    'button_background_color': 'light grey',
+    'active_foreground_color': 'black',
+    'disabled_foreground_color': 'white',
+    'highlight_background': 'ivory2',
+    'highlight_color': 'ivory2',
+    'highlight_thickness': 2
+}
---
+++ English version/music_analysis.py
@@ -0,0 +1,144 @@
+key_header = 'key: '
+
+
+def grammar_translate(current):
+    global key_header
+    result = ''
+    interval = 1
+    comments = ''
+    show_bar = 'T'
+    bar_line_character = '|'
+    arrow_character = '→ '
+    show_chord_analysis = 'T'
+    if current[:2] == 'k.':
+        current_key = current[2:]
+        result += f'{key_header}{current_key}'
+        return result
+    elif current[:2] == 'k!':
+        new_key_header = current[2:]
+        if new_key_header:
+            key_header = new_key_header
+        return
+    parts = current.split('$')
+    length = len(parts)
+    if length == 1:
+        bar_chords = parts[0]
+        chord_degrees = '.'
+    elif length == 2:
+        bar_chords, chord_degrees = parts
+    elif length > 2:
+        bar_chords, chord_degrees = parts[:2]
+        other_parts = parts[2:]
+        for each in other_parts:
+            flag = each[:2]
+            content = each[2:]
+            if flag == 'i=':
+                try:
+                    interval = int(content)
+                except:
+                    pass
+            elif flag == 'c=':
+                comments = content
+            elif flag == 'b=':
+                show_bar = content
+            elif flag == 's=':
+                if content.split():
+                    bar_line_character = content
+            elif flag == 'a=':
+                if content:
+                    arrow_character = content
+            elif each[:3] == 'ca=':
+                show_chord_analysis = each[3:]
+    else:
+        return
+    bar_chords_split = bar_chords.split(';')
+    if show_bar == 'T':
+        bar_num = bar_chords_split[0]
+        current_chords = bar_chords_split[1:]
+        if len(bar_num) >= 3:
+            if bar_num[0] == '[' and bar_num[-1] == ']':
+                current_chords_len = len(current_chords)
+                if bar_num[-2] == '*':
+                    bar_num_list = [
+                        bar_num[1:-2] for i in range(current_chords_len)
+                    ]
+                else:
+                    bar_num_list = bar_num[1:-1].split(',')
+                    if len(bar_num_list) != current_chords_len:
+                        return
+                return '\n\n'.join([
+                    grammar_translate('$'.join([
+                        f"{bar_num_list[k]};{';'.join(['!'+current_chords[i] if i == k else current_chords[i] for i in range(current_chords_len)])}"
+                    ] + parts[1:])) for k in range(current_chords_len)
+                ])
+
+        result += f'{bar_num}\n'
+    else:
+        current_chords = bar_chords_split
+    current_play = False
+    current_play_num = 0
+    for i in range(len(current_chords)):
+        each = current_chords[i]
+        if each:
+            if each[0] == '!':
+                current_play = True
+                current_play_num = i
+                current_chords[i] = each[1:]
+    if chord_degrees in ['.', '~', '']:
+        show_chord_analysis = 'F'
+    chord_degrees = chord_degrees.split(';')
+
+    current_bar_chords = []
+    current_bar_chords_degree = []
+    degree_inds = []
+    chord_inds = []
+    current_chord_num = len(current_chords)
+    chord_degrees_num = len(chord_degrees)
+    for k in range(current_chord_num):
+        chord_name = current_chords[k]
+        if current_play and k == current_play_num:
+            chord_name = arrow_character + chord_name
+        result += f'{chord_name}{" "*interval}{bar_line_character}{" "*interval}'
+    if current_chord_num == 0:
+        return
+    bar_line_character_len = len(bar_line_character)
+    recent_line = result[result.rfind('\n') + 1:]
+    result = result[:(-2 * interval - bar_line_character_len)]
+    if show_chord_analysis == 'T':
+        result += '\n'
+        for current_ind in range(chord_degrees_num):
+            chord_degree = chord_degrees[current_ind]
+            if current_ind == 0:
+                inds = 0
+                if current_play and current_play_num == 0:
+                    inds += len(arrow_character)
+                result += ' ' * inds + chord_degree
+                chord_inds = [
+                    j + interval + bar_line_character_len
+                    for j in range(len(recent_line))
+                    if recent_line[j:j + bar_line_character_len] ==
+                    bar_line_character
+                ]
+                if current_play and current_play_num != 0:
+                    chord_inds[current_play_num - 1] += len(arrow_character)
+            else:
+                try:
+                    inds = chord_inds[current_ind - 1]
+                except:
+                    return
+                last_degree = chord_degrees[current_ind - 1]
+                last_degree_len = len(last_degree)
+                last_chord = current_chords[current_ind - 1]
+                result += ' ' * (inds - degree_inds[-1] -
+                                 last_degree_len) + chord_degree
+            degree_inds.append(inds)
+    if comments:
+        comments = comments.replace('\\n', '\n')
+        result += f'\n{comments}'
+    return result
+
+
+def whole_translate(text, interval=2):
+    results = [grammar_translate(i) if i else '' for i in text.split('\n')]
+    results = [i for i in results if i is not None]
+    return '\n'.join(results)
